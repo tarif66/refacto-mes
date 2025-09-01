9e61848e44961d0f9a05a440abf566c5553d67d7
tarif66 <obrechon@gmail.com>
2025-09-01 16:34:00 +0200

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Accueil from '../routes/Accueil.vue'
+import ProfilMedical from '../routes/Profil-medical.vue'
+import Vaccinations from '../routes/Vaccinations.vue'
+import Documents from '../routes/Documents.vue'
+import Messagerie from '../routes/Messagerie.vue'
+import Prevention from '../routes/Prevention.vue'
+import Agenda from '../routes/Agenda.vue'
+import CatalogueDeServices from '../routes/Catalogue de services.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,8 +16,42 @@ const router = createRouter({
           path: '/',
           name: 'root',
           component: Accueil,
-        }
-
+        },
+        { 
+          path: '/profil-medical',
+          name: 'profil-medical',
+          component: ProfilMedical,
+        },
+        { 
+          path: '/vaccinations',
+          name: 'vaccinations',
+          component: Vaccinations,
+        },
+        { 
+          path: '/documents',
+          name: 'documents',
+          component: Documents,
+        },
+        { 
+          path: '/messagerie',
+          name: 'messagerie',
+          component: Messagerie,
+        },
+        { 
+          path: '/prevention',
+          name: 'prevenetion',
+          component: Prevention,
+        },
+        { 
+          path: '/agenda',
+          name: 'agenda',
+          component: Agenda,
+        },
+        { 
+          path: '/catalogue-de-services',
+          name: 'catalogue-de-services',
+          component: CatalogueDeServices,
+        },
       ],
         scrollBehavior() {
     return { top: 0 }
